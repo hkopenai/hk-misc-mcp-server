128a0217189b94aa081f9efed2ee08641b116054
Neo Alienson <neo@01man.com>
2025-07-18 21:03:20 +0800

    refactor

--- hkopenai/hk_misc_mcp_server/__main__.py
+++ hkopenai/hk_misc_mcp_server/__main__.py
@@ -5,7 +5,7 @@ This module serves as the entry point to start the MCP server.
 """
 
 from hkopenai_common.cli_utils import cli_main
-from . import server
+from .server import server
 
 if __name__ == "__main__":
     cli_main(server, "HK Misc MCP Server")
